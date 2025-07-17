256b2d939c5a853eecca6d4d7f5c9eec2828ad53
igormelz <igorm@openfs.ru>
2025-07-17 16:09:31 +0300

    dev 1.0.9: refactoring resources

--- src/main/kotlin/ru/openfs/lbapi/resource/AuthResource.kt
+++ src/main/kotlin/ru/openfs/lbapi/resource/AuthResource.kt
@@ -8,16 +8,16 @@ import ru.openfs.lbapi.api3.GetVersion
 import ru.openfs.lbapi.api3.Logout
 import ru.openfs.lbapi.api3.TVersion
 import ru.openfs.lbapi.exception.ApiException
-import ru.openfs.lbapi.service.SoapClientService
+import ru.openfs.lbapi.service.adapter.SoapAdapter
 
 @GraphQLApi
 class AuthResource(
-    private val clientService: SoapClientService,
+    private val soapAdapter: SoapAdapter,
 ) {
 
     @Query
     fun getApiReady(): Boolean = try {
-        clientService.withSession().request<TVersion> { GetVersion() }.version.isNotEmpty()
+        soapAdapter.noSession().request<TVersion> { GetVersion() }.version.isNotEmpty()
     } catch (_: ApiException) {
         false
     }
@@ -28,9 +28,9 @@ class AuthResource(
         @Name("password") password: String
     ): String {
         Log.info("Try login: $login")
-        val answer = clientService.startSession(login, password)
-        Log.info("Successfully login $login with sessionId: [$answer]")
-        return answer
+        val sessionId = soapAdapter.startClientSession(login, password)
+        Log.info("Successfully login $login with sessionId: [$sessionId]")
+        return sessionId
     }
 
     @Query
@@ -38,7 +38,7 @@ class AuthResource(
         @Name("sessionId") sessionId: String
     ): String {
         Log.info("Try logout: $sessionId")
-        clientService.withSession(sessionId).request<String> { Logout() }
+        soapAdapter.withSession(sessionId).request<String> { Logout() }
         Log.info("Successfully logout [$sessionId]")
         return "Bye"
     }
